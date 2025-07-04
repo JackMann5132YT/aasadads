671a8c2553dd342c96638a19673ea5e4de016f0f
JackMann5132YT <connorsj100@icloud.com>
2025-07-03 20:27:20 -0400

    Update code.java

--- code.java
+++ code.java
@@ -1,2 +1,12 @@
-# aasadads
-gaots
+Pokedex > catch pidgey
+Throwing a Pokeball at pidgey...
+pidgey was caught!
+You may now inspect it with the inspect command.
+Pokedex > catch caterpie
+Throwing a Pokeball at caterpie...
+caterpie was caught!
+You may now inspect it with the inspect command.
+Pokedex > pokedex
+Your Pokedex:
+ - pidgey
+ - caterpie
